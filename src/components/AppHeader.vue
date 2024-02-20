94680460d3d289c611c7e8aadc7e90c8eb1ad45e
ChristianBaciu <christian3110@outlook.it>
2024-02-20 12:10:56 +0100

    App Header sistemato

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -33,7 +33,7 @@
                         // false
                     },
                     {
-                        // nome: '<i class="fa-solid fa-cart-shopping"></i>'
+                        nome: '<i class="fa-solid fa-cart-shopping"></i>'
                         // false
                     }
                 ]
@@ -47,41 +47,31 @@
 <template>
     <header class="d-flex justify-content-between">
         <div class="experienceHeader">
-
             <div class="titleLogo">
                 <img class="ms-5" src="../assets/img/logo-restaurant-2x-600x115.png" width="250" alt="">
             </div>
 
             <div class="dining mx-auto text-light">
-                
-                
-                <div class="ciao ">
+                <div class="barra pe-5">
                     <span></span>
                     <h5>the best table in town</h5>
                 </div>    
 
-
-
-
-                <div>
+                <div class="testo">
                     <h1>fine dining experience</h1>
                     <br>
                     <p>Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.</p>
                     <br>
                     <button class="button-black">explore the menu</button>
                 </div>
-
-
-
-
             </div>
-            
         </div>
 
         <div class="imgHeader">
             <ul class="titleLogo d-flex gap-4 justify-content-center p-4 my-1 text-light">
                 <li v-for="(element, index) in headerAvada" :key="index">
-                    {{ element.nome }}
+                    <span v-if="element.nome.includes('<i')" v-html="element.nome"></span>
+                    <span v-else>{{ element.nome }}</span>
                 </li>
             </ul>
             <!-- <img src="../assets/img/slider52x.jpg" alt=""> -->
@@ -92,36 +82,38 @@
 <!-- ----------------------------------------------------------------------------- -->
 
 <style lang="scss" scoped>
-    ul{
-        list-style-type: none;
-    }
-
     header{
 
         .experienceHeader{
-            width: 47%;
             background-color: black;
+            width: 47%;
             height: 95vh;
 
-            .ciao{
-                display: flex;
-                align-items: center;
-
-                span{
-                    width: 40px;
-                    height: 1px;
-                    background-color: white;
-                    margin-right: 40px;
-                }
-            }
-
-
-
-
             .dining{
-                width: 350px;
-                padding: 20px;
+                width: 400px;
                 margin-top: 100px;
+
+                .testo{
+                    padding-top: 35px;
+                    margin-left: 10px;
+                    padding-left: 47px;
+                }
+
+                .barra{
+                    display: flex;
+                    align-items: center;
+
+                    span{
+                        background-color: white;
+                        width: 40px;
+                        height: 1px;
+                        margin-right: 20px;
+                    }
+
+                    h5{
+                        margin: 0;
+                    }
+                }
             }
         }
 
@@ -133,6 +125,10 @@
             width: 53%;
             height: 95vh;
             margin: 0;
+
+            ul{
+                list-style-type: none;
+            }
         }
 
         .titleLogo{
